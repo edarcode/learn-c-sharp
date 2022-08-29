1cd4f87e9d207423a0eb0882eaa6f5c52a7e1a6e
edarcode <edarcode@gmail.com>
2022-08-28 22:49:24 -0500

    feat() aprendiendo a crear métodos estaticos

---
+++ learn-c-sharp/App.cs
@@ -0,0 +1,16 @@
+﻿internal class App
+{
+    private static void Main(string[] args)
+    {
+        addNumbers(2, 8);
+    }
+    static void showMsgByConsole(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+    static void addNumbers(int num1,int num2)
+    {
+        string result = $"{num1} + {num2} = {num1 + num2}";
+        showMsgByConsole(result);
+    }
+}
--- learn-c-sharp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Calcular el area de un circulo. Debe recibir por consola el radio.
-
-Console.WriteLine("Digite el radio del circulo");
-
-string inputRadio =  Console.ReadLine();
-double radio = double.Parse(inputRadio);
-double area = Math.Pow(radio,2) * Math.PI;
-
-Console.WriteLine($"El área de un círculo con radio {radio} es: {area}");
