9785b55c490abb90b3b119f76d8d428263608057
edarcode <edarcode@gmail.com>
2022-08-28 21:09:36 -0500

    feat() calculando área de un círculo

--- learn-c-sharp/Program.cs
+++ learn-c-sharp/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hola edarcode");
+﻿// Calcular el area de un circulo. Debe recibir por consola el radio.
+
+Console.WriteLine("Digite el radio del circulo");
+
+string inputRadio =  Console.ReadLine();
+double radio = double.Parse(inputRadio);
+double area = Math.Pow(radio,2) * Math.PI;
+
+Console.WriteLine($"El área de un círculo con radio {radio} es: {area}");
