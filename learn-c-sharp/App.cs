e7bb0fd58dbeeeaaeb5432534683747ed0f16714
edarcode <edarcode@gmail.com>
2022-08-28 22:53:43 -0500

    refactor() el metodo addNumbers ahora es mas legible

--- learn-c-sharp/App.cs
+++ learn-c-sharp/App.cs
@@ -10,7 +10,8 @@
     }
     static void addNumbers(int num1,int num2)
     {
-        string result = $"{num1} + {num2} = {num1 + num2}";
-        showMsgByConsole(result);
+        int sumResult = num1 + num2;
+        string msg = $"{num1} + {num2} = {sumResult}";
+        showMsgByConsole(msg);
     }
 }
